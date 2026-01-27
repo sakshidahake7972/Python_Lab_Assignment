8743bd09f1d2a012220ffc3f1b5ce93137220b9a
sakshidahake7972 <dahakesakshi43@gmail.com>
2026-01-27 17:58:15 +0530

    this is a pyramid question

--- p2.py
+++
@@ -1,5 +0,0 @@
-n=5
-for i in range(1,n+1):
-    for j in range(i):
-        print(i, end=" ")
-    print()
--- p5.py
+++
@@ -1,6 +0,0 @@
-num=2
-for i in range(1,5):
-    for j in range(i):
-        print(num, end=" ")
-        num+=2
-    print()
