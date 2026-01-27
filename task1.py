4c0734ba02d0f76ce6a57943be45c2c122a95ee1
sakshidahake7972 <dahakesakshi43@gmail.com>
2026-01-27 16:24:18 +0530

    In this include a pyramid questions

---
+++ task1.py
@@ -0,0 +1,39 @@
+# #1
+# n=5
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(j, end=" ")
+#     print()
+
+#2
+# n=5
+# for i in range(1,n+1):
+#     for j in range(i):
+#         print(i, end=" ")
+#     print()
+
+#3
+# n=5
+# for i in range(1,n+1):
+#     for j in range(i,0,-1):
+#         print(j, end=" ")
+#     print()
+
+#4
+# n=5
+# for i in range(1,n+1):
+#     for j in range(1,i+1):
+#         print(j%2, end=" ")
+#     print()
+
+#5
+# num=2
+# for i in range(1,5):
+#     for j in range(i):
+#         print(num, end=" ")
+#         num+=2
+#     print()
+
+#6
+# for i in range(1,6):
+#     print("*"*i)
