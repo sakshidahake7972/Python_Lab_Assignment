90ea9e7002fa06c20605e449775206b54ffc28cf
sakshidahake7972 <dahakesakshi43@gmail.com>
2026-02-01 12:44:38 +0530

    this is a code of charecter pyramid

---
+++ task2.py
@@ -0,0 +1,29 @@
+
+# for i in range(1, 6):
+#     for j in range(i):
+#         print(chr(65 + j), end="")
+#     print()
+
+
+
+# for i in range(1, 6):
+#     for j in range(1, i + 1):
+#         if j % 2 == 0:
+#             print("#", end="")
+#         else:
+#             print(".", end="")
+#     print()
+
+
+# char = ['p', 'y', 't', 'h', 'o', 'n']
+# for i in range(len(char)):
+#     print(char[i] * (i + 1))
+
+
+# word = "python"
+# for i in range(1, len(word) + 1):
+#     print(word[:i])
+
+
+
+
