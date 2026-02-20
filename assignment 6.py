6ad09f594dc5e46b1a87aa294538ee5c0b0ea0af
sakshidahake7972 <dahakesakshi43@gmail.com>
2026-02-20 18:03:23 +0530

    this code for a practice

---
+++ assignment 6.py
@@ -0,0 +1,37 @@
+# Lab Assignment - 1
+
+# string = input("Enter a string: ")
+#
+# vowels = 0
+# consonants = 0
+# spaces = 0
+# lowercase = 0
+#
+# for ch in string:
+#     if ch in "aeiouAEIOU":
+#         vowels += 1
+#     elif ch.isalpha():
+#         consonants += 1
+#     elif ch == " ":
+#         spaces += 1
+#
+#     if ch.islower():
+#         lowercase += 1
+#
+# print("\nString Statistics:")
+# print("Number of Vowels:", vowels)
+# print("Number of Consonants:", consonants)
+# print("Number of Spaces:", spaces)
+# print("Number of Lowercase Letters:", lowercase)
+
+
+# Lab Assignment - 2
+
+# sentence = input("Enter a sentence: ")
+#
+# capitalized_sentence = sentence.upper()
+#
+# print("\nOutput:")
+# print(capitalized_sentence)
+
+
